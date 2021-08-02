8a329a835ecce63139358613ebce420ef5ab3f9f
Sagleft <33496565+Sagleft@users.noreply.github.com>
2021-08-02 14:26:12 +0400

    Create cron.go

---
+++ golang/cron.go
@@ -0,0 +1,22 @@
+package main
+
+type cronObject struct {
+	TimerTime time.Duration
+	Callback  cronCallback
+}
+
+func newCronHandler(callback cronCallback, timerTime time.Duration) cronObject {
+	return cronObject{
+		TimerTime: timerTime,
+		Callback:  callback,
+	}
+}
+
+type cronCallback func()
+
+func (c *cronObject) run() {
+	for true {
+		c.Callback()
+		time.Sleep(c.TimerTime)
+	}
+}
